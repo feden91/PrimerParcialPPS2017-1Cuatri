7b8f7f468fe95fb865ffbfa56a55aca7aca1f442
feden91 <fede_n_91@hotmail.com>
2017-04-20 22:31:39 -0300

    aplicacion de firebase sin poder pegarle a la base todavia.

--- Trivia/Trivia/src/pages/home/home.ts
+++ Trivia/Trivia/src/pages/home/home.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController } from 'ionic-angular';
+
+//import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+// task: FirebaseListObservable<any>
+  //
+  constructor(public navCtrl: NavController) {}
 }
+    //public alertController:AlertController, public database: AngularFireDatabase
+//this.user=this.database.list(*/usuarios);
+
+ // }
+
+ //createUser(){
+ //let newUserModal = this.alertController.create({
+ //title:"Nuevo Usuario",
+ //message:"Agregar",
+
+ //inputs:
+ //[{
+   //  name:"nombre",
+   //  placeholder:"nombre"
+
+ //}],
+
+  //buttons:[{
+
+  // text:"Guardar",
+  // handler: data =>{
+
+ //console.log('Guardar click');
+
+  // }
+
+ // }]
+
+ // //})
+
+
+ //}
