fdc96ca81ef3f228f74e55b78a4501119493dcde
feden91 <fede_n_91@hotmail.com>
2017-04-22 15:05:38 -0300

    problemas con import {AngularFire, FirebaseListObservable} from 'angularfire2';

--- Trivia/Trivia/src/pages/home/home.ts
+++ Trivia/Trivia/src/pages/home/home.ts
@@ -1,46 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController,AlertController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
-//import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2';
+import {AngularFire,FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-// task: FirebaseListObservable<any>
-  //
-  constructor(public navCtrl: NavController) {}
+  usuario:string
+  mensaje:string
+
+  listadoDeMensajes: FirebaseListObservable<any[]>;
+
+  constructor(public navCtrl: NavController,af:AngularFire) {this.listadoDeMensajes=af.database.list('*/mensajes');
+
+this.usuario="anonimo";  
 }
-    //public alertController:AlertController, public database: AngularFireDatabase
-//this.user=this.database.list(*/usuarios);
-
- // }
-
- //createUser(){
- //let newUserModal = this.alertController.create({
- //title:"Nuevo Usuario",
- //message:"Agregar",
-
- //inputs:
- //[{
-   //  name:"nombre",
-   //  placeholder:"nombre"
-
- //}],
-
-  //buttons:[{
-
-  // text:"Guardar",
-  // handler: data =>{
-
- //console.log('Guardar click');
-
-  // }
-
- // }]
-
- // //})
-
-
- //}
+}
+  
