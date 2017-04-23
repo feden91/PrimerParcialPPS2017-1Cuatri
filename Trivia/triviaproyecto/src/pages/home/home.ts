0bd95fc864a472646ca772d5896cde542d606ca9
feden91 <fede_n_91@hotmail.com>
2017-04-23 14:17:15 -0300

    funciona base de datos

---
+++ Trivia/triviaproyecto/src/pages/home/home.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  usuario:string
+  mensaje:string
+
+  listadoDeMensajes: FirebaseListObservable<any[]>;
+
+
+  constructor(public navCtrl: NavController,af:AngularFire) {
+
+    this.listadoDeMensajes=af.database.list('/mensajes');
+    this.usuario="anonimo";
+
+  }
+EnviarMensaje(){
+
+this.listadoDeMensajes.push({usuario:this.usuario,mensaje:this.mensaje});
+this.mensaje="";
+
+
+}
+}
