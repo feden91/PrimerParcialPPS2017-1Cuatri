fbbcbf9259fa351a850fd7cd6ef80d4deefec0fb
feden91 <fede_n_91@hotmail.com>
2017-04-28 21:59:20 -0300

    se puso login en trivia

--- Trivia/triviaproyecto/src/pages/home/home.ts
+++ Trivia/triviaproyecto/src/pages/home/home.ts
@@ -1,44 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
-import {juegoPage } from '../juego/juego';
-import { MyData } from '../../providers/my-data';
+//import { ppot} from '../PiedraPapelOTijera/ppot';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+usuario = { nombre:'',
+        Puntuacion:0,
+        gano:false};
 
-  Usuario:string
-  private preguntasJSON:any = [];
+        user : FirebaseListObservable<any[]>;
 
-  listadoDeUsuarios: FirebaseListObservable<any[]>;
-
-
-  constructor(public navCtrl: NavController,af:AngularFire, private servicio: MyData) {
-
-this.servicio.traerPreguntas().subscribe(
-      datos => this.preguntasJSON = datos,
-      error => console.error(error));
-    
-
+  constructor(public navCtrl: NavController,af: AngularFire) {
+this.user= af.database.list('/Usuarios');
   }
-Acceder(Usuario){
+Usuario
 
-this.Usuario=Usuario;
-this.navCtrl.push(juegoPage, {
-      Usuario : this.Usuario,
-      preguntasArray: this.preguntasJSON
-    });
-
-//this.listadoDeUsuarios.push({Usuario:this.Usuario});
-//this.mensaje="";
-
-
-}
-
-
- 
-
+         Ingreso(){
+           
+          this.user.push({
+            nombre:this.usuario.nombre
+         })
+         //this.navCtrl.push(ppot,this.usuario)  
+        }
 }
