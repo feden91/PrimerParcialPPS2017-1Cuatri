e34b8a1514c21ed5a75a9a83ed3a476d27c03161
feden91 <fede_n_91@hotmail.com>
2017-05-19 23:28:18 -0300

    se agrego metodo para leer la variable que guarda el file native.

--- teclado/src/pages/teclado/teclado.ts
+++ teclado/src/pages/teclado/teclado.ts
@@ -10,6 +10,7 @@ import { File } from '@ionic-native/file';
 export class teclado {
 Sonido:any;
 num:any;
+var:any;
   constructor(public navCtrl: NavController,private nativeAudio: NativeAudio,private vibration: Vibration,private file: File) {
  console.log(file.dataDirectory);
 this.nativeAudio.preloadSimple('ratas', 'assets/sound/Ratas.mp3');
@@ -25,7 +26,7 @@ this.nativeAudio.preloadSimple('mono', 'assets/sound/Mono.mp3');
 ratas(){
 
   this.Sonido=" this.nativeAudio.play('ratas', () => console.log('ratas is done playing'));";
-  this.guardado(this.Sonido);
+  this.guardado("ratas");
 
    
   this.nativeAudio.play('ratas', () => console.log('ratas is done playing'));
@@ -34,79 +35,53 @@ this.vibration.vibrate(500);
 }
 grillo  (){
  this.Sonido=" this.nativeAudio.play('grillo', () => console.log('grillo is done playing'));";
-    this.guardado(this.Sonido);
+    this.guardado("grillo");
   this.nativeAudio.play('grillo', () => console.log('grillo is done playing'));
   this.vibration.vibrate(500);
 }
 mono  (){
  this.Sonido=" this.nativeAudio.play('mono', () => console.log('mono is done playing'));";
-    this.guardado(this.Sonido);
+    this.guardado("mono");
   this.nativeAudio.play('mono', () => console.log('mono is done playing'));
  this.vibration.vibrate(500);
 }
 murcielago (){
  this.Sonido=" this.nativeAudio.play('murcielago', () => console.log('murcielago is done playing'));";
-    this.guardado(this.Sonido);
+    this.guardado("murcielago");
   this.nativeAudio.play('murcielago', () => console.log('murcielago is done playing'));
 this.vibration.vibrate(500);
 }
 pajaro (){
  this.Sonido=" this.nativeAudio.play('pajaro', () => console.log('pajaro is done playing'));";
-   this.guardado(this.Sonido);
+   this.guardado("pajaro");
   this.nativeAudio.play('pajaro', () => console.log('pajaro is done playing'));
 this.vibration.vibrate(500);
 }
 mosca (){
  this.Sonido=" this.nativeAudio.play('mosca', () => console.log('mosca is done playing'));";
-    this.guardado(this.Sonido);
+    this.guardado("mosca");
   this.nativeAudio.play('mosca', () => console.log('mosca is done playing'));
 this.vibration.vibrate(500);
 }
 
 guardado(d){
-if(this.num>0 && this.num<3)
-{
- this.num=this.num+1;
 
- if(this.num==1){
-
-this.file.writeFile(this.file.externalDataDirectory,"Boton1.php",d,true);
-
- }
-if(this.num==2){
-
-this.file.writeFile(this.file.externalDataDirectory,"Boton2.php",d,true);
-
- }
- if(this.num==3){
-
-this.file.writeFile(this.file.externalDataDirectory,"Boton3.php",d,true);
-
- }
-
+this.Destructor1();
+this.file.writeFile(this.file.externalDataDirectory,"Boton1.txt",d,true);
 
 }
 
-}
-
-
+Destructor1(){
+           this.file.removeFile(this.file.externalDataDirectory,"Boton1.txt");
+      }
 boton1(){
 
-this.file.readAsText(this.file.externalDataDirectory, "Boton1.php");
-
-
+this.var=this.file.readAsText(this.file.externalDataDirectory, "Boton1.txt");
+if(this.var=="ratas")
+{
+ this.nativeAudio.play('ratas', () => console.log(' playing'));
 }
-boton2(){
-
-this.file.readAsText(this.file.externalDataDirectory, "Boton2.php");
-
-
 }
-boton3(){
 
-this.file.readAsText(this.file.externalDataDirectory, "Boton3.php");
-
-
-}
 
 }
